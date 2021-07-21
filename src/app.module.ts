b240b9538c6fc18ec7a0a9fe09235f43f1cb6a27
chainvisions <chainvisions@outlook.com>
2021-07-20 19:46:19 -0600

    Use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PriceController } from './price/price.controller';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, PriceController],
   providers: [AppService],
 })
